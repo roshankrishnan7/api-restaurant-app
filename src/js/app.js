613bca855026abfa9ea980a916045e0131a3d618
roshan <roshan@dyn-118-139-16-66.its.monash.edu.au>
2017-05-02 15:30:21 +1000

    [roshan] revert to basic API

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var stormpath = require('express-stormpath');
+//var stormpath = require('express-stormpath');
 
 var app = express();
 
 //console.log('Welcome to Meal-O server');
 
-
+/*
 app.use(stormpath.init(app, {
   expand: {
     customData: true,
@@ -15,9 +15,9 @@ app.use(stormpath.init(app, {
   }
 }));
 
-
-app.get('/item', stormpath.apiAuthenticationRequired, function(req, res){
-  res.json({menu: req.user.customData.notes || "This is your food item"});
+*/
+app.get('/item', function(req, res){
+  res.json({menu: "This is your food item"});
 });
 
 app.listen(3000);
