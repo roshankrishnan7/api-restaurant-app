1fae688e6f4016077405c97ef1dcc73c3aed66ed
roshan <roshan@localhost.localdomain>
2017-05-02 10:08:05 +1000

    [roshan] creates basic REST API

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
-console.log('Welcome to Meal-O server');
+var express = require('express');
+
+var app = express();
+
+//console.log('Welcome to Meal-O server');
+
+app.get('/item', function(req, res){
+  res.json({menu: "This is your food item"});
+});
+
+
+app.listen(3000);
