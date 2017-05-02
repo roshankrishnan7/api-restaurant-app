80adc49957d7caed9463dac35eac5f0a8b0188a6
roshan <roshan@localhost.localdomain>
2017-05-02 10:18:47 +1000

    [roshan] adds basic stormpath authentication

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,23 @@
 var express = require('express');
+var stormpath = require('express-stormpath');
 
 var app = express();
 
 //console.log('Welcome to Meal-O server');
 
-app.get('/item', function(req, res){
-  res.json({menu: "This is your food item"});
-});
 
+app.use(stormpath.init(app, {
+  expand: {
+    customData: true,
+  },
+  web: {
+    produces: ['application/json']
+  }
+}));
+
+
+app.get('/item', stormpath.apiAuthenticationRequired, function(req, res){
+  res.json({menu: req.user.customData.notes || "This is your food item"});
+});
 
 app.listen(3000);
