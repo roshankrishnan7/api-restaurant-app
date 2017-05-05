327ae7f149e3f22d81c1f5625a2fbfb8bec54485
roshankrishnan <roshankrishnan7@gmail.com>
2017-05-05 20:43:58 +1000

    [roshan] adds initial routes

---
+++ api/routes/meal-o-routes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app){
+  var mealO = require('../controllers/meal-o-controller');
+
+//meal-o routes
+app.route('/items')
+   .get(mealO.list_all_items);
+
+app.route('/orders')
+    .put(mealO.createOrder);
+
+app.route('/tasks/:itemId')
+   .get(mealO.displayItem)
+   .put(mealO.addItemToCart);
+};
